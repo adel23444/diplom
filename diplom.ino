1bfa5c6f81c3448451282f49961ce14656444d35
khamidullin.a <khamidullin.a@etton.ru>
2023-04-17 00:24:34 +0300

    Тестирование датчиков и повторителей поворота без таймера

--- diplom.ino
+++ diplom.ino
@@ -1,51 +1,127 @@
 #include <NewPing.h>
 
-#define PIN_TRIG_LEFT 5
-#define PIN_ECHO_LEFT 6
+byte PIN_TRIG_LEFT = 5;
+byte PIN_ECHO_LEFT = 6;
 
-#define PIN_TRIG_RIGHT 3
-#define PIN_ECHO_RIGHT 4
+byte PIN_TRIG_RIGHT = 3;
+byte PIN_ECHO_RIGHT = 4;
 
-#define PIN_TRIG_PRIMARY 10
-#define PIN_ECHO_PRIMARY 9
+byte PIN_TRIG_PRIMARY = 7;
+byte PIN_ECHO_PRIMARY = 8;
 
-#define MAX_DISTANCE 30
+byte MAX_DISTANCE = 30;
 
 NewPing sonar_left(PIN_TRIG_LEFT, PIN_ECHO_LEFT, MAX_DISTANCE);
 NewPing sonar_right(PIN_TRIG_RIGHT, PIN_ECHO_RIGHT, MAX_DISTANCE);
 NewPing sonar_primary(PIN_TRIG_PRIMARY, PIN_ECHO_PRIMARY, MAX_DISTANCE);
 
 
-byte LED_BACK=13;
-byte LED_RIGHT=12;
-byte LED_LEFT=11;
+byte LED_BACK=12;
+byte LED_RIGHT=11;
+byte LED_LEFT=10;
+
+
+// volatile unsigned long int timerLED1;
+// unsigned long int timerLED1Loop;
+// volatile bool timerLED1On;
+// bool LEDOn;
+// ISR (TIMER1_COMPA_vect)
+// {
+//   if(timerLED1On)
+//   	timerLED1++;
+// }
 
 void setup() {
   // put your setup code here, to run once:
-  delay(1000);
   pinMode(LED_BACK, OUTPUT);
   pinMode(LED_RIGHT, OUTPUT);
   pinMode(LED_LEFT, OUTPUT);
   Serial.begin(9600);
+
+  //Установки Таймер2: Делитель частоты /8, режим 0
+//Частота = 16MHz/8 = 2Mhz или 0.5 мкс
+//Делитель /8 дает нам хороший рабочий диапазон
+//так что сейчас мы просто жестко запрограммируем это.
+
+  // TCCR1A = (1<<WGM11); // сброс по совпадению
+  // TCCR1B |= 1<<CS10 | 1<<CS11 | 0<<CS12; 
+
+  // TIMSK1 |= (1<< OCIE1A); //подключение прерывания по переполнению 
+  //  sei(); //запуск прерываний
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   unsigned int distanceSm_left = sonar_left.ping(); // Создание сигнала, получение параметра его продолжительности в мкс (uS).
   unsigned int distance_left = distanceSm_left / US_ROUNDTRIP_CM;
-  Serial.print("Ping: ");
+  Serial.print("Ping LEFT: ");
   Serial.print(distance_left); // Пересчет параметра времени в величину расстояния и вывод результата (0 соответствует выходу за допустимый предел)
-  Serial.println("cm");
+  Serial.println(" cm");
+  delay(50);
 
-  if (distance_left < 20) {
+  unsigned int distanceSm_right = sonar_right.ping(); // Создание сигнала, получение параметра его продолжительности в мкс (uS).
+  unsigned int distance_right = distanceSm_right / US_ROUNDTRIP_CM;
+  Serial.print("Ping RIGHT: ");
+  Serial.print(distance_right); // Пересчет параметра времени в величину расстояния и вывод результата (0 соответствует выходу за допустимый предел)
+  Serial.println(" cm");
+  delay(50);
+
+  // unsigned int distanceSm_primary = sonar_primary.ping(); // Создание сигнала, получение параметра его продолжительности в мкс (uS).
+  // unsigned int distance_primary = distanceSm_primary / US_ROUNDTRIP_CM;
+  // Serial.print("Ping PRIMARY: ");
+  // Serial.print(distance_primary); // Пересчет параметра времени в величину расстояния и вывод результата (0 соответствует выходу за допустимый предел)
+  // Serial.println(" cm");
+  // delay(50);
+  if ((distance_left > 0 && distance_right > 0) && distance_left < 10 && distance_right < 10) {
+    digitalWrite(LED_BACK, HIGH);
+    }
+  else if (distance_left > 0 && distance_left < 10) {
+    digitalWrite(LED_BACK, LOW);
     byte i = 0;
     while (i < 10) {
       digitalWrite(LED_RIGHT, HIGH);
       delay(300);
       digitalWrite(LED_RIGHT, LOW);
       delay(300);
-    i++;
+      i++;
     }
   }
-  delay(1000);
+
+  else if (distance_right > 0 && distance_right < 10) {
+    digitalWrite(LED_BACK, LOW);
+    byte i = 0;
+    while (i < 10) {
+      digitalWrite(LED_LEFT, HIGH);
+      delay(300);
+      digitalWrite(LED_LEFT, LOW);
+      delay(300);
+      i++;
+    }
+  }
+  else {
+    digitalWrite(LED_BACK, LOW);
+  }
+  
+
+  // timerLED1On = 1;
+  // cli();
+  // timerLED1Loop = timerLED1;
+  // sei();
+  // if(timerLED1Loop >= 1500)
+  // {
+  //   if(LEDOn == 0)
+  //   {
+  //     LEDOn = 1;
+  //     digitalWrite(13, LEDOn);
+  //   }
+  //   else
+  //   {
+  //     LEDOn = 0;
+  //     digitalWrite(13, LEDOn);
+  //   }
+  //   cli();
+  //   timerLED1=0;
+  //   sei();
+  // }
+  delay(300);
 }
