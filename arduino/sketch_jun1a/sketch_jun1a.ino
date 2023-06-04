33ec10c884f0a8a003d191f72f9a701006361b70
khamidullin.a <khamidullin.a@etton.ru>
2023-06-04 17:59:44 +0300

    Пробы подключения к MQTT

--- arduino/sketch_jun1a/sketch_jun1a.ino
+++ arduino/sketch_jun1a/sketch_jun1a.ino
@@ -1,10 +1,18 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-IPAddress mqttServer(192, 168, 0, 106);
-const char* SSID = "malcon";
-const char* PWD = "1234fav1234";
+IPAddress mqttServer(185, 152, 81, 104);
+const char* SSID = "xiaomi11lite";
+const char* PWD = "favorit123";
 long lastime = 0;
-
+String command_str = "";
+boolean recievedFlag;
+int tm = 300;
+#define ROBOT_NAME "ROBOT1"
+enum signals {
+  LEFT: "WAITSENL",
+  RIGHT: "WAITSENR",
+  PRIMARY: "WAITSENP"
+}
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println("Callback");
   Serial.println((char)payload[0]);
@@ -17,8 +25,8 @@ void connectToWiFi() {
   Serial.println(SSID);
   WiFi.begin(SSID, PWD);
   while (WiFi.status() != WL_CONNECTED) {
-    
-    Serial.print("Cannot connect!");
+
+    Serial.println("Cannot connect!");
     delay(500);
     // we can even make the ESP32 to sleep
   }
@@ -37,36 +45,40 @@ void reconnect() {
     if (mqttClient.connect(clientId.c_str())) {
       Serial.println("Connected.");
       // subscribe to topic
+      mqttClient.subscribe("robot/test");
+      mqttClient.publish("robot/test", (String(ROBOT_NAME)+"WI1").c_str());
+      delay(200);
     }
   }
 }
-
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   connectToWiFi();
 }
-
+void DataSend(String command) {
+  if (tm = 0) {
+    
+    Serial.println("Publishing data..");
+    mqttClient.publish("robot/test", command.c_str(), command.length());
+    Serial.println(command_str);
+    tm = 300;  // пауза меду отправками значений температуры коло 3 секунд
+  }
+  tm--;
+  delay(10);
+}
 void loop() {
-
-  Serial.println("CONNECTED!!!");
 
   if (!mqttClient.connected())
     reconnect();
   mqttClient.loop();
+  while (Serial.available()) {
+    char symbol = (char)Serial.read();
+    command_str += String(symbol);
 
-
-  // Publishing data to MQTT
-  long now = millis();
-  if (now - lastime > 10000) {
-    Serial.println("Publishing data..");
-    // read temperature from sensor
-    mqttClient.publish("robot/test", "hello_temp");
-    delay(200);
-    // read humidity from sensor
-    mqttClient.publish("robot/test", "hello_humidity");
-    lastime = now;
+    delay(5);
   }
-  delay(300);
+  command_str = String(ROBOT_NAME) + command_str;
+  recievedFlag = true;
+  DataSend(command_str);
+  command_str = "";
 }
-
-
--- arduino/sketch_jun1c/sketch_jun1c.ino
+++ arduino/sketch_jun1c/sketch_jun1c.ino
@@ -1,34 +1,37 @@
 #include <SoftwareSerial.h>
- 
+
 // создаём объект для работы с программным Serial
 // и передаём ему пины TX и RX
-SoftwareSerial mySerial(8, 9);
- 
+SoftwareSerial mySerial(2, 3);
+
 // serial-порт к которому подключён Wi-Fi модуль
-#define WIFI_SERIAL    mySerial
- 
-void setup()
-{
+#define WIFI_SERIAL mySerial
+
+String command;
+
+void setup() {
   // открываем последовательный порт для мониторинга действий в программе
   // и передаём скорость 9600 бод
   Serial.begin(9600);
-  while (!Serial) {
-  // ждём, пока не откроется монитор последовательного порта
-  // для того, чтобы отследить все события в программе
-  }
   Serial.print("Serial init OK\r\n");
   // открываем Serial-соединение с Wi-Fi модулем на скорости 115200 бод
   WIFI_SERIAL.begin(115200);
 }
- 
-void loop()
-{
-  // если приходят данные из Wi-Fi модуля - отправим их в порт компьютера
-  if (WIFI_SERIAL.available()) {
-    Serial.write(WIFI_SERIAL.read());
+
+void loop() {
+  while (!WIFI_SERIAL.readString() == "WAITSENL") {
+    command = "SENL" + String(25);
   }
-  // если приходят данные из компьютера - отправим их в Wi-Fi модуль
-  if (Serial.available()) {
-    WIFI_SERIAL.write(Serial.read());
+
+
+  WIFI_SERIAL.println(command);
+  delay(300);
+  while (!WIFI_SERIAL.readString() == "WAITSENR") {
+    command = "SENR" + String(18);
   }
+  WIFI_SERIAL.println(command);
+  delay(1000);
+  command = "SENP" + String(39);
+  WIFI_SERIAL.println(command);
+  delay(1000);
 }
